c1add49f294a3b4fadb3e0f249c4166a52410bf5
Benjamin T. Seaver and Jason Brown <jason@>
2017-02-16 11:49:04 -0800

    add input to failure message

--- tests/ScrabbleTest.php
+++ tests/ScrabbleTest.php
@@ -8,18 +8,35 @@
         {
             // Arrange
             $scrabble_class = new Scrabble();
-            $input = "the";
-            $expected_result = 6;
 
-            // Act
-            $actual_result = $scrabble_class->wordScore($input);
+            $tests = array(
+                'quick' => 20,
+                'brown' => 10,
+                'fox' => 13,
+                'jumps' => 16,
+                'over' => 7,
+                'the' => 6,
+                'lazy' => 16,
+                'dog' => 5
+            );
 
-            // Assert
-            $this->assertEquals($expected_result, $actual_result);
+            foreach ($tests as $input => $expected_output) {
+                // Arrange
+                $expected_result = array(
+                    'input' => $input,
+                    'output' => $expected_output
+                );
+
+                // Act
+                $actual_output = $scrabble_class->wordScore($input);
+                $actual_result = array(
+                    'input' => $input,
+                    'output' => $actual_output
+                );
+
+                // Assert
+                $this->assertEquals($expected_result, $actual_result);
+            }
         }
     }
-
-
-
-
 ?>
